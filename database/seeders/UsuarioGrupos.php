a17588c6ce8c69ab43a68caf47381e8b89852d72
Milton Ojeda <https://github.com/ojedamilton>
2023-02-04 12:27:10 -0300

    Creacion Usuario-Grupo

---
+++ database/seeders/UsuarioGrupos.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\UserGrupo;
+
+class UsuarioGrupos extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $arrUserGrupo =[
+            ['idUsuario' => 1, 'idGrupo'=>1,'estadoUsuarioGrupos'=>1]
+        ] ;
+       $grupo= new UserGrupo();
+       $grupo->insert($arrUserGrupo);
+    }
+}
