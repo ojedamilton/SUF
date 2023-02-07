36d1b23a25c655786c6b3424aa54add276bd6809
Adrincovich <adrian.drincovich@gmail.com>
2023-02-07 19:55:29 -0300

    Traduccion modulo de seguridad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,7 +52,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <a class="dropdown-item" href="{{ route('logout') }}"
              onclick="event.preventDefault();
                            document.getElementById('logout-form').submit();">
-              {{ __('Logout') }}
+              {{ __('Cerrar Sesión') }}
           </a>
 
           <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
