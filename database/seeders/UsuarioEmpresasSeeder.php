0d54c03313a59fd6ed60d36653c10c677c166b5a
adrincovich <adrian.drincovich@gmail.com>
2023-05-31 00:33:29 -0300

    Correccion seeder

--- database/seeders/UsuarioEmpresasSeeder.php
+++ database/seeders/UsuarioEmpresasSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\usuarioEmpresa;
 
-class UsuarioEmpresas extends Seeder
+class UsuarioEmpresasSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeders/UsuarioGruposSeeder.php
+++ database/seeders/UsuarioGruposSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\UserGrupo;
 
-class UsuarioGrupos extends Seeder
+class UsuarioGruposSeeder extends Seeder
 {
     /**
      * Run the database seeds.
